42b8c87ab85f57241b8ec62cd45acf1ad9ad6571
anthem53 <anthem53@postech.ac.kr>
2018-10-16 21:20:15 -0700

    open close master

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -6,21 +6,34 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+
+struct file_decripter{
+  void * file;
+  char name[15];
+  bool is_open;
+}; 
 
 
+struct file_decripter fd[100];
 
 static void syscall_handler (struct intr_frame *);
 static void verify_pointer(struct intr_frame * esp);
-
+static int insert_fd_list(void * file, char * name);
 
 static void exit(int status);
 static bool create(const char * file, unsigned initial_size);
+static int open (char * file);
+static void close(int fd);
 static int write(int fd, void * buf, int size);
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  memset(fd,0,100);
+
 }
 
 static void
@@ -58,15 +71,14 @@ syscall_handler (struct intr_frame *f)
 
       bool result = create((char*)arg[0],(unsigned)arg[1]);
       f -> eax = (uint32_t)result;
-//      printf(">>> create result : %d\n",result);
-  //    printf(">>> craete eax : %d \n",esp->eax);
-      //printf(">>> create \n");
       break;
     case SYS_REMOVE :
       printf(">>> remove \n");
       break;
     case SYS_OPEN :
-      printf(">>> open \n");
+      arg[0] = *((int *)esp + 1);
+      int result_fd = open((char*)arg[0]);
+      f->eax = result_fd;
       break;
     case SYS_FILESIZE :
       printf(">>> filesize \n");
@@ -90,8 +102,8 @@ syscall_handler (struct intr_frame *f)
       printf(">>> tell \n");
       break;
     case SYS_CLOSE :
-
-      printf(">>> close \n");
+      arg[0] = *((int*)esp + 1);
+      close(arg[0]);
   }
 
 
@@ -113,6 +125,20 @@ static void verify_pointer(struct intr_frame * esp){
 
 }
 
+static int insert_fd_list(void * file, char * name){
+  int i =0;
+
+  for(i = 2 ; i < 100; i++){
+    if(fd[i].is_open == false){
+      fd[i].file = file;
+      strlcpy(fd[i].name, name , 14);
+      fd[i].is_open = true;
+      return i;
+      break;
+    }
+  }
+
+}
 
 static void exit(int status)
 {
@@ -157,6 +183,43 @@ static bool create(const char * file, unsigned initial_size){
   /* already existed*/
   bool result = filesys_create(file, initial_size);
   return result;  
+
+}
+
+static int open(char * file)
+{
+  if (file == NULL)
+    exit(-1);
+  if(pagedir_get_page(thread_current()->pagedir,(void*)file) == NULL)
+    exit(-1);
+
+  int len = strlen(file);
+  if(len == 0)
+    return -1;
+  void * file_address = filesys_open(file);
+  
+  if(file_address == NULL)
+    return -1;  
+  else {
+    int result = insert_fd_list(file_address,file);
+    return result;
+  }
+
+}
+
+static void close(int this_fd)
+{
+  if (this_fd < 2)
+    return;
+  if (this_fd < 0 ||this_fd > 100)
+    return;  
+
+  if(fd[this_fd].is_open == true){
+    file_close(fd[this_fd].file);
+    fd[this_fd].file = NULL;
+    fd[this_fd].name[0] = '\0';
+    fd[this_fd].is_open = false;
+  }
 
 }
 
